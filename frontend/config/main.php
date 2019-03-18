5f6ab7b70473204a945c72bdd4fad63fb57ef480
semenov <semenov@skeeks.com>
2019-03-18 16:09:55 +0300

    dev

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -15,14 +15,14 @@ $config = [
         'view' => [
             'theme' => [
                 'class' => \v3project\themes\mega\ThemeMega::class,
-                'logo' => 'http://4.bp.blogspot.com/-pybOnS5ISC4/Ts6oFp9r9LI/AAAAAAAABMM/4gCTGO3OCcA/s1600/DOROT%25C3%2589IA.png',
+                /*'logo' => 'http://4.bp.blogspot.com/-pybOnS5ISC4/Ts6oFp9r9LI/AAAAAAAABMM/4gCTGO3OCcA/s1600/DOROT%25C3%2589IA.png',
                 'favicon' => 'http://4.bp.blogspot.com/-pybOnS5ISC4/Ts6oFp9r9LI/AAAAAAAABMM/4gCTGO3OCcA/s1600/DOROT%25C3%2589IA.png',
                 'title' => 'Куклы ТУТ',
                 'h1_title' => 'Интернет-магазин "Куклы ТУТ"',
                 'site_nav_color' => '#e06993',
                 'btn_color' => '#e06993',
                 'a_color' => '#e06993',
-                'bg_image' => 'https://sorokavorona.com.ua/4293-fullwidth_product/kukla-alina-govorit-poet-pesnyu-rost-kukly-26-sm-4-vida.jpg',
+                'bg_image' => 'https://sorokavorona.com.ua/4293-fullwidth_product/kukla-alina-govorit-poet-pesnyu-rost-kukly-26-sm-4-vida.jpg',*/
             ],
         ],
     ],
